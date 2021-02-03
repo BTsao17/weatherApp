62ce0452dd9fed12304d64a1fcef8bdf64158b72
Bonnie Tsao <IceJade1788@gmail.com>
2021-02-03 01:29:39 -0800

    fetch weather data from API using hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { weather_KEY } from './keys';
 
 function App() {
+  const [ location, setLocation ] = useState({ lat: '49.163168', lon: '123.13741' });
+  const [ forecast, setForecast ] = useState([]);
+
+  useEffect(
+    () => {
+      const { lat, lon } = location;
+      fetch(
+        `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=-${lon}&units=metric&exclude=minutely,hourly,alerts&APPID=${weather_KEY}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setForecast([data]);
+        });
+    },
+    [ location ] //only re-run this effect if location changes
+  );
+
+  console.log(forecast);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Weather Forecast</h1>
+      <main>
+        <section>
+          <p>search bar</p>
+        </section>
+        <section>
+          <h3>Richmond, BC</h3>
+          <section>
+            <p>Currently:</p>
+          </section>
+          <p>days of week</p>
+        </section>
+      </main>
     </div>
   );
 }
